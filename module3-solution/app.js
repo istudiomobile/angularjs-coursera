dc8ee02289443ab3bed1c6196943697c04f2c3a1
Alfredo Ramírez <Alfredo Ramrez>
2017-04-02 19:43:07 -0500

    Added week 3 code.

---
+++ module3-solution/app.js
@@ -0,0 +1,52 @@
+(function(){
+
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+    
+    .controller('NarrowItDownController', ['MenuSearchService', function(MenuSearchService){
+
+        //---- Variables ----
+        var vm = this;
+
+        //---- Methods ----
+        vm.searchForDescription = function(description){
+            MenuSearchService.getMatchedMenuItems(description).then(function(items){
+                vm.foundItems = items;
+            });            
+        };
+
+        vm.removeItem = function(pointer){
+            vm.foundItems.splice(pointer, 1);
+        };
+
+    }])
+
+    .service('MenuSearchService', ['$http', function($http){
+
+        //---- Methods ----
+        this.getMatchedMenuItems = function(word){
+            return $http({method:'GET', url:'https://davids-restaurant.herokuapp.com/menu_items.json'}).then(function (result) {
+                // process result and only keep items that match
+                return result.data.menu_items.filter((item) => {return item.description.search(word) !== -1});              
+            });        
+        };
+        
+
+    }])
+
+    .directive('foundItems', function(){
+
+        var ddo = {
+            templateUrl: 'descriptionsList.html',
+            scope:{
+                items: '<',
+                onRemove: '&'
+            }
+        };
+
+        return ddo;
+
+    });
+
+})();
